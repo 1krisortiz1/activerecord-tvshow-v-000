73f23f22e01c44bdca9e6bd56fb1896dd36d6a40
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-03 14:51:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,16 @@
 class Show < ActiveRecord::Base
+    def self.highest_rating
+    end
+
+    def self.most_popular_show
+    end
+
+    def self.lowest_rating
+    end
+
+    def self.least_popular_show
+    end
+
+    def self.ratings_sum
+    end
 end
